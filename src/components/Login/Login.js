da8b1ae4d30c2d6c0332c607b9402d75b3734d9b
1amSumit <sj779619@gmail.com>
2023-04-07 10:33:22 +0530

    context api

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -20,6 +20,9 @@ const passwordReducer = (state, action) => {
   if (action.type === "INPUT_PASSWORD") {
     return { value: action.val, isValid: action.val.trim().length > 6 };
   }
+  if (action.type === "INPUT_PASSWORD_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
   return { value: "", isValid: false };
 };
 
